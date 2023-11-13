c1cba24f04870243e6f04384b84db101302c789d
Collins <oriakhicolls01@gmail.com>
2023-11-13 17:24:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eden_demo
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/eden_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eden_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
